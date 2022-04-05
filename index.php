a8f8392dab5a48d301393a70698b54387023b4bf
Yosshii50 <24950665+yosshii50@users.noreply.github.com>
2022-04-05 09:42:52 +0000

    Update

--- index.php
+++ index.php
@@ -6,8 +6,6 @@ $data = file_get_contents( "index.base.html" );
 $body = "内容";
 $javascript = "JS";
 
-
-
 $data = str_replace( "{%title%}" , $Title , $data );
 $data = str_replace( "{%body%}" , $body , $data );
 $data = str_replace( "{%javascript%}" , $javascript , $data );
